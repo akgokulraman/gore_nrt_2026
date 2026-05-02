84787ecdd06a5c69698741710f8188e2b60e0807
Gokul Raman (Darwin) <gokul@udel.edu>
2026-05-02 18:33:11 -0400

    feat(analysis): analysis on emulsion stability through PE and clustersize
    - with three configurations data plot the mean of the data
    - use standard deviation (obtained from the three configurations data) to fill the plot
    - incase, there is no three configurations present, only one case of data, just plot the mean

---
+++ notebooks/thermo_analysis.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import os, sys
+from io import StringIO
+
+DIR = sys.argv[1] # raw data directory
+ENSEMBLE = sys.argv[2] # dictates if equilibration_NPT, production_NVT
+FIGURES = sys.argv[3] # directory to store figures
+SUBDIR = sys.argv[4] # subdirectory under DIR/ENSEMBLE
+dir_name = DIR.split("/")[-1]
+# figure storage location
+if SUBDIR == '*':
+    figure_save_location = f"{FIGURES}/{dir_name}/{ENSEMBLE}" 
+else:
+    figure_save_location = f"{FIGURES}/{dir_name}/{ENSEMBLE}/{SUBDIR}" 
+NOTEBOOK_DIR = '/lustre/ea-nrtmidas/users/3770/emulsion_stability/notebooks'
+os.chdir(NOTEBOOK_DIR)
+
+def filter_comments(file_path):
+    """
+    Generator to read a file and clubs lines that do not start 
+    with '#' or '@'.
+    """
+    not_commented = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            if not line.strip().startswith('#') and not line.strip().startswith('@'):
+                not_commented.append(line)
+    return not_commented
+
+def compute_mean_std(dfs, time_col, value_col):
+
+    values_interp = []
+
+    for df in dfs:
+        t = df[time_col].values
+        v = df[value_col].values
+
+        values_interp.append(v)
+    if(len(values_interp)>1):
+        # presence of multiple configurations
+        values_interp = np.array(values_interp)
+        mean = values_interp.mean(axis=0)
+        std = values_interp.std(axis=0)
+    else:
+        # only one configuration
+        mean = values_interp[0]
+        std = np.zeros_like(mean)
+    return t, mean, std
+
+# plotting styles
+config_marker = ['+', 'd', 's']
+config_marker_dict = {'config-3':'+', 'config-2':'d', 'config-1':'s'}
+line_styles = ['-','--', '-.', ':', (0, (5, 10)), (0, (3, 10, 1, 10)), (0, (3, 5, 1, 5, 1, 5)), (0, (5, 5)), (0, (5, 1))]
+
+plt.style.use('seaborn-v0_8-whitegrid')  # clean base style
+
+plt.rcParams.update({
+    'font.size': 16,            # base font
+    'axes.labelsize': 20,       # x/y labels
+    'axes.titlesize': 18,       # subplot titles
+    'xtick.labelsize': 16,      # x tick numbers
+    'ytick.labelsize': 16,      # y tick numbers
+    'legend.fontsize': 16,      # legend text
+    'legend.title_fontsize': 15,
+    'lines.markersize': 6,
+    'figure.titlesize': 20
+})
+
+file_thermo = 'thermo/thermo.xvg'
+file_cluster = 'cluster/my_num_clusters.xvg'
+configs_eq_thermo_list = []
+configs_eq_cluster_list = []
+config_list_thermo = []
+config_list_cluster = []
+
+# themo file data collection
+for f in glob.glob(f'{DIR}/{ENSEMBLE}/{SUBDIR}/{file_thermo}'):
+    config = f.split('/')[-3]
+    conf_list = config_list_thermo.append(config)
+    # Use StringIO to treat the filtered lines as a file for read_csv
+    removed_comments = filter_comments(f'{f}')
+    file_removed_comments = StringIO('\n'.join(removed_comments))
+    df = pd.read_csv(file_removed_comments, header=None, sep='\s+', names=['time (ps)', 'potential', 'kinetic', 'total E', 'T', 'P', 'density'])
+    configs_eq_thermo_list.append(df)
+
+# no. cluster file data collection
+for f in glob.glob(f'{DIR}/{ENSEMBLE}/{SUBDIR}/{file_cluster}'):
+    config = f.split('/')[-3]
+    config_list_cluster.append(config)
+    # Use StringIO to treat the filtered lines as a file for read_csv
+    removed_comments = filter_comments(f'{f}')
+    file_removed_comments = StringIO('\n'.join(removed_comments))
+    df = pd.read_csv(file_removed_comments, header=None, sep='\s+', names=['Time (ps)', 'Number of Clusters'])
+    configs_eq_cluster_list.append(df)
+
+# define intervals between the data points for thermodynamic data and number of clusters
+interval_thermo = 10
+interval_cluster = 10
+'''
+Plot PE and Number of Clusters vs time
+'''
+fig, axes = plt.subplots(2, 1, figsize=(7,6), sharex=True)
+
+# Potential Energy
+t_pe, pe_mean, pe_std = compute_mean_std(
+    configs_eq_thermo_list,
+    'time (ps)',
+    'potential'
+)
+axes[0].plot(t_pe[interval_thermo::interval_thermo], pe_mean[interval_thermo::interval_thermo], color='tab:blue', label='PE')
+if(len(configs_eq_thermo_list)>1): # denoting multiple configurations
+    axes[0].fill_between(
+        t_pe[interval_thermo::interval_thermo],
+        pe_mean[interval_thermo::interval_thermo] - pe_std[interval_thermo::interval_thermo],
+        pe_mean[interval_thermo::interval_thermo] + pe_std[interval_thermo::interval_thermo],
+        color='tab:blue',
+        alpha=0.3
+    )
+
+# Clusters
+t_cl, cl_mean, cl_std = compute_mean_std(
+    configs_eq_cluster_list,
+    'Time (ps)',
+    'Number of Clusters'
+)
+axes[1].plot(t_cl[interval_cluster::interval_cluster], cl_mean[interval_cluster::interval_cluster], color='tab:orange', label='Clusters count')
+if(len(configs_eq_thermo_list)>1): # denoting multiple configurations
+    axes[1].fill_between(
+        t_cl[interval_cluster::interval_cluster],
+        cl_mean[interval_cluster::interval_cluster] - cl_std[interval_cluster::interval_cluster],
+        cl_mean[interval_cluster::interval_cluster] + cl_std[interval_cluster::interval_cluster],
+        color='tab:orange',
+        alpha=0.3
+    )
+
+axes[0].set_ylabel('Potential Energy \n(kJ/mol)')
+axes[0].grid(alpha=0.2)
+axes[1].set_xlabel('Time (ps)')
+axes[1].set_ylabel('Number of Clusters')
+axes[1].grid(alpha=0.2)
+fig.legend(bbox_to_anchor=(1.0, 1.0))
+plt.tight_layout()
+os.makedirs(figure_save_location, exist_ok=True)
+fig.savefig(f'{figure_save_location}/PE_Clusters.png', bbox_inches="tight")
