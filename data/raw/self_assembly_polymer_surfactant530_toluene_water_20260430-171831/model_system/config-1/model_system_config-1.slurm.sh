10a3789326537d61f1a46989a10906372d511cb2
Gokul Raman (Darwin) <gokul@udel.edu>
2026-05-13 12:36:17 -0400

    feat(data): include simulation directory for polymer-solvent-surfactant530-water with initial model as mixture undergoing self-assembly
    contains used batch scripts, .itp, .mdp, .top files

---
+++ data/raw/self_assembly_polymer_surfactant530_toluene_water_20260430-171831/model_system/config-1/model_system_config-1.slurm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=idle
+#SBATCH --account=ea-nrtmidas
+#SBATCH --time=00:10:00
+#SBATCH --job-name="model_system_config-1"
+#SBATCH --output="model_system_config-1.out"
+#SBATCH --error="model_system_config-1.err"
+#SBATCH --comment "Model the initial configuration of the system. Create three initial configurations to ensure the validity, reproducibility, and statistical significance of the simulation results."
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#UD_QUIET_JOB_SETUP=YES
+#UD_MACHINE_FILE_FORMAT='%h%[:]C'
+#export UD_JOB_EXIT_FN_SIGNALS="SIGTERM EXIT"  
+#UD_QUIET_JOB_SETUP=YES
+#UD_MACHINE_FILE_FORMAT='%h%[:]C'
+#export UD_JOB_EXIT_FN_SIGNALS="SIGTERM EXIT"  
+
+# adding necessary packages
+vpkg_require gromacs  
+. /opt/shared/slurm/templates/libexec/openmpi.sh  
+vpkg_require conda
+conda activate md
+
+# copy relavant files
+cp -r /lustre/ea-nrtmidas/users/3770/emulsion_stability/data/input/polymer_surfactant_solvent/itp .
+mkdir top
+cp /lustre/ea-nrtmidas/users/3770/emulsion_stability/data/input/polymer_surfactant_solvent/top/surfactant-530_polymer-20_toluene-12460_water_conf1.top top
+cp  /lustre/ea-nrtmidas/users/3770/emulsion_stability/data/input/polymer_surfactant_solvent/*.gro .
+
+# create topology of the system
+srun -n 1 -N 1 --mpi=pmix gmx insert-molecules -ci surfactant.gro -nmol 530  -box 23.0 23.0 23.0 -o surfactant530_conf1.gro
+srun -n 1 -N 1 --mpi=pmix gmx insert-molecules -ci polymer.gro -nmol 20 -f surfactant530_conf1.gro -o polymer20_surfactant530_conf1.gro
+srun -n 1 -N 1 --mpi=pmix gmx insert-molecules -ci toluene.gro -nmol 12460 -f polymer20_surfactant530_conf1.gro -o toluene12460_polymer20_surfactant530_conf1.gro
+
+# solvate the system with water
+srun -n 1 -N 1 --mpi=pmix gmx solvate -cp toluene12460_polymer20_surfactant530_conf1.gro -cs water.gro -o solvated_surfactant-530_polymer-20_toluene-12460_water_conf1.gro -p top/surfactant-530_polymer-20_toluene-12460_water_conf1.top
+
